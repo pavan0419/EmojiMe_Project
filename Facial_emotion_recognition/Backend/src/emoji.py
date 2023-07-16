9363a9e25e77ea31e99c689d0f68f9baa764971e
Pawan Kumar <pkpavanp512@gmail.com>
2023-07-16 20:25:38 +0530

    final changes applied

--- Facial_emotion_recognition/Backend/src/emoji.py
+++ Facial_emotion_recognition/Backend/src/emoji.py
@@ -11,7 +11,6 @@ from keras.preprocessing.image import ImageDataGenerator
 import threading
 
 
-
 emotion_model = Sequential()
 emotion_model.add(Conv2D (32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
 emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
